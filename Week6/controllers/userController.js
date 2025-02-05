9dd0cc08b4b3b47d8082f39f3693dc889cc21f85
Tufayl Dalvi <tufayldalvi007@gmail.com>
2025-02-06 01:02:17 +0530

    Week 6 overview done

--- Week6/controllers/userController.js
+++ Week6/controllers/userController.js
@@ -1,9 +1,42 @@
-const signup = async () => {
-  console.log("Hello from signup")
+const jwt = require("jsonwebtoken")
+const users = []
+
+const SECRET_KEY = process.env.JWT_SECRET
+const signup = async (req, res) => {
+  const { username, password } = req.body
+  if (!username || !password) {
+    return res.json({ status: 400, message: "Enter Credentials" })
+  }
+
+  users.push({ username, password })
+  return res.json({ status: 200, message: "Welcome" })
 }
 
-const signin = async () => {
-  console.log("Hello from signin")
+const signin = async (req, res) => {
+  const { username, password } = req.body
+  if (!username || !password) {
+    return res.send({ status: 400, message: "Enter Credentials" })
+  }
+  const user = users.find(
+    (u) => u.username === username && u.password === password,
+  )
+
+  if (user) {
+    const token = jwt.sign({ username: username }, SECRET_KEY)
+    return res.json({ token, message: "Logged in" })
+  }
 }
 
-module.exports = { signin, signup }
+const getMyInfo = async (req, res) => {
+  const token = req.headers.token
+  if (!token) {
+    return res.json({ message: "Unauthorized Access" })
+  } else {
+    const decodedInformation = jwt.verify(token, SECRET_KEY)
+    const username = decodedInformation.username
+    return res.json({
+      message: `Hello ${username}`,
+    })
+  }
+}
+module.exports = { signin, signup, getMyInfo }
--- Week6/routes/routes.js
+++ Week6/routes/routes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const { signin, signup } = require("../controllers/userController.js")
+const { signin, signup, getMyInfo } = require("../controllers/userController.js")
 router.post("/signup", signup)
 
 router.post("/signin", signin)
 
+router.get("/me", getMyInfo)
 module.exports = router
